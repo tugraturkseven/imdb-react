cb119d067c2f3ae840a791f3b648c7837f01ebe4
Tugra <tugraturkseven@hotmail.com>
2023-04-26 13:51:58 +0300

    ui updates, bug fixes, responsive design.

--- src/components/Cards/Card.js
+++ src/components/Cards/Card.js
@@ -8,14 +8,20 @@ function Card() {
         axios({
             method: 'GET',
             url: 'https://moviesdatabase.p.rapidapi.com/titles',
-            params: { limit: '50' },
+            params: {
+                list: 'most_pop_movies',
+                limit: '50',
+                startYear: '2005'
+            },
             headers: {
+                'content-type': 'application/octet-stream',
                 'X-RapidAPI-Key': '531544e39amsh6145987e479baf9p104408jsnc6a3e9e4ba60',
                 'X-RapidAPI-Host': 'moviesdatabase.p.rapidapi.com'
             }
         }).then(res => {
             setMovies(res.data.results);
             setLoading(false);
+
         })
     }, [])
 
@@ -25,15 +31,19 @@ function Card() {
         )
     } else {
         return (
-            <div className="grid grid-cols-5 bg-slate-900">
+            <div className="grid xl:grid-cols-5 md:grid-cols-3 sm:grid-cols-2 lg:grid-cols-4  bg-slate-900">
                 {
-                    movies.map(element => (
-                        <div key={element?.id} className="max-w-xs h-80 rounded-lg overflow-hidden bg-slate-800 shadow-2xl m-5">
-                            <img className="w-full" src={element.primaryImage?.url} alt="" />
-                            <p className="font-bold font-sans text-xl text-center text-slate-100 my-3">{element.titleText?.text}</p>
-                            <p className="rounded-lg bg-slate-700 text-slate-300 text-center w-16 my-3 m-auto">#{element.releaseYear?.year}</p>
-                        </div>
-                    ))
+                    movies.map(element => {
+                        if (typeof element.primaryImage?.url == "string") {
+                            return (
+                                <div key={element?.id} className="w-64 h-96 min-h-96 rounded-lg overflow-hidden shadow-2xl m-5 hover:cursor-pointer mx-auto" >
+                                    <img className="w-full max-h-80" src={element.primaryImage?.url} alt="" />
+                                    <p className="font-bold font-sans text-xl md:text-md sm:text-sm   text-center text-slate-100 -bottom-10 w-full">{element.titleText?.text}</p>
+                                </div>
+                            )
+                        } else {
+                        }
+                    })
                 }
             </div>
         )
