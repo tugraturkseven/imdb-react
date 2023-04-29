345683b5642ec8f6e2f370eabf9cee55ca970d76
Tugra <tugraturkseven@hotmail.com>
2023-04-29 10:00:47 +0300

    ui updates, series cards, router.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import Navbar from './components/navbar/Navbar'
-import Card from './components/Cards/Card'
-import React, { useState } from 'react'
+import MovieCard from './components/Cards/MovieCard'
+import SeriesCard from './components/Cards/SeriesCard'
+import React from 'react'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
 
   return (
-    <div className="bg-slate-900 h-screen">
+    <div className="max-h-screen">
       <Navbar />
-      <Card />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<MovieCard />} />
+          <Route path="series" element={<SeriesCard />} />
+          <Route path="movies" element={<MovieCard />} />
+        </Routes>
+      </BrowserRouter>
     </div >
   )
 }
--- src/components/Cards/SeriesCard.js
+++ src/components/Cards/SeriesCard.js
@@ -3,15 +3,17 @@ import axios from 'axios';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faHeartBroken } from '@fortawesome/free-solid-svg-icons'
 
-function Card() {
+
+function SeriesCard() {
     const [loading, setLoading] = useState(true);
-    const [movies, setMovies] = useState([]);
+    const [series, setSeries] = useState([]);
+
     useEffect(() => {
         axios({
             method: 'GET',
             url: 'https://moviesdatabase.p.rapidapi.com/titles',
             params: {
-                list: 'most_pop_movies',
+                list: 'most_pop_series',
                 limit: '50',
                 startYear: '2005'
             },
@@ -21,7 +23,7 @@ function Card() {
                 'X-RapidAPI-Host': 'moviesdatabase.p.rapidapi.com'
             }
         }).then(res => {
-            setMovies(res.data.results);
+            setSeries(res.data.results);
             setLoading(false);
 
         })
@@ -33,18 +35,19 @@ function Card() {
         )
     } else {
         return (
-            <div className="grid xl:grid-cols-5 md:grid-cols-3 sm:grid-cols-2 lg:grid-cols-4  bg-slate-900">
+            <div className="grid xl:grid-cols-5 md:grid-cols-3 sm:grid-cols-2 lg:grid-cols-4  bg-gradient-to-r from-slate-900 via-purple-900 to-slate-900">
                 {
-                    movies.map(element => {
+                    series.map(element => {
                         if (typeof element.primaryImage?.url == "string") {
                             return (
-                                <div key={element?.id} className="relative w-64 h-96 min-h-80 rounded-lg overflow-hidden shadow-2xl m-5 hover:cursor-pointer mx-auto" >
+                                <div key={element?.id} className="relative w-64 h-fit min-h-80 rounded-lg overflow-hidden shadow-2xl m-5 hover:cursor-pointer mx-auto" >
                                     <FontAwesomeIcon icon={faHeartBroken} className="absolute top-3 right-3 text-lg hover:text-xl drop-shadow-xl text-white  bg-slate-700  p-2 rounded-full "></FontAwesomeIcon>
-                                    <img className="w-full max-h-80" src={element.primaryImage?.url} alt="" />
-                                    <p className="font-bold font-sans text-xl md:text-md sm:text-sm   text-center text-slate-100 -bottom-10 w-full">{element.titleText?.text}</p>
+                                    <img className="w-full h-80 object-cover" src={element.primaryImage?.url} alt="" />
+                                    <p className="font-bold font-sans text-xl md:text-md sm:text-sm  mb-5 text-center text-slate-100 -bottom-10 w-full">{element.titleText?.text}</p>
                                 </div>
                             )
                         } else {
+
                         }
                     })
                 }
@@ -54,4 +57,4 @@ function Card() {
 
 }
 
-export default Card;
+export default SeriesCard
