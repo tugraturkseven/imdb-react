f8896f1ae2fe48bc2aa39a9f6301cd18087e1bb5
Tugra <tugraturkseven@hotmail.com>
2023-05-25 22:55:18 +0300

    favorites page ui bug fixes, api base info update.

--- src/components/Cards/Favorites.js
+++ src/components/Cards/Favorites.js
@@ -23,6 +23,9 @@ function Favorites() {
                 const response = await axios({
                     method: 'GET',
                     url: `https://moviesdatabase.p.rapidapi.com/titles/${id}`,
+                    params: {
+                        info: 'base_info'
+                    },
                     headers: {
                         'X-RapidAPI-Key': '531544e39amsh6145987e479baf9p104408jsnc6a3e9e4ba60',
                         'X-RapidAPI-Host': 'moviesdatabase.p.rapidapi.com'
@@ -49,7 +52,7 @@ function Favorites() {
         )
     } else {
         return (
-            <div className="grid xl:grid-cols-5 md:grid-cols-3 sm:grid-cols-2 lg:grid-cols-4 bg-gradient-to-r from-slate-900 via-purple-900 to-slate-900">
+            <div className="grid xl:grid-cols-5 md:grid-cols-3 sm:grid-cols-2 lg:grid-cols-4 bg-gradient-to-r from-slate-900 via-purple-900 to-slate-900 min-h-screen">
                 {elementData.map(element => {
                     const id = element?.id;
                     const imgUrl = element?.primaryImage?.url;
